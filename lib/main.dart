4503d239717d37882a3c4aa930cfd6b381615ec9
unknown <viiktoor@gmail.com>
2020-10-04 14:08:26 -0400

    set up basics

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello retangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'hello',
+            style: TextStyle(fontSize: 32.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
